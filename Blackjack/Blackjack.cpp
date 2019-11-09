4c6c6a56e0897783adcfc5f3fee9bb061a2f67f5
stavilam <stavilam@coventry.ac.uk>
2019-11-09 18:15:29 +0000

    mysterious commit message

--- Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack.cpp
@@ -26,17 +26,19 @@ struct Card {
 class Hand {
 public:
 	//attributes
-	std::string owner = "Player";
+	std::string owner = "PLAYER";
 	std::string type = "hard";
 	std::vector<Card> cards;
+	int aceCount = 0;
 
 	//methods
 	void draw(std::vector<Card>& deck) {
 		this->cards.push_back(deck.back());
-		if (deck.back().value == 1) {
-			this->calculateSoftHand();
+		if (deck.back().name.find("Ace") != std::string::npos) {
+			this->aceCount++;
 		}
 		deck.pop_back();
+		bestSoftHand();
 	}
 
 	void deal(std::vector<Card>& deck) {
@@ -46,26 +48,31 @@ public:
 	}
 
 	void show(bool visible = 1) {	
-		if (visible == 1) {	
-			std::cout << this->owner << " hand: ";
-			int total = 0;
+		if (visible == 1 || this->owner == "PLAYER") {	
+			std::cout << this->owner;
+			std::cout << " CARDS (" << this->cards.size() << ", " << this->type << "): ";
 			for (const Card& card : this->cards) {
 				std::cout << "[" << card.name << "] ";
-				total += card.value;
 			}
-			std::cout << "## TOTAL: " << total << " [" << this->type << "]" << std::endl;
+			std::cout << std::endl;
+			std::cout << this->owner;
+			std::cout << " TOTAL: " << this->total();
 		}
-		else if (visible == 0) {
-			std::cout << this->owner << " hand: ";
+		else if (visible == 0 || this->owner == "DEALER") {
+			std::cout << this->owner;
+			std::cout << " CARDS (" << this->cards.size() << "): ";
 			std::cout << "[" << this->cards[0].name << "] ";
 			for (int i = 0; i < this->cards.size() - 1; i++) {
 				std::cout << "[ ? ] ";
 			}
-			std::cout << "##   TOTAL: " << this->cards[0].value;
+			std::cout << std::endl;
+			std::cout << this->owner;
+			std::cout << " TOTAL: " << this->cards[0].value;
 			for (int i = 0; i < this->cards.size() - 1; i++) {
 				std::cout << " + ?";
 			}
 		}
+		std::cout << std::endl;
 	}
 
 	int total() {
@@ -80,27 +87,33 @@ public:
 		cards.clear();
 	}
 
-	void calculateSoftHand() {
-		this->type = "soft";
-		int aceCount = 0;
-		size_t handSize = this->cards.size(); //size_t to avoid "possible loss of data"
-		for (int i = 0; i < handSize; i++) {
-			if (this->cards[i].value == 1) {
-				aceCount++;
-			}
-		}
-		int total = this->total();
+	void bestSoftHand() {
+		if (this->aceCount == 0) { return; }                 //if hand has no aces return
+		if (this->type == "hard") { this->type = "soft"; }   //change handtype to soft if it's hard
 
-		//calculate best hand
-		//as long as there are aces in the hand, and converting next ace won't bust
-		//find the next ace in the hand, and change its value to 11
-		while (aceCount-- && (total + 10 <= 21) ) {
-			for (int i = 0; i < handSize; i++) {
-				if (this->cards[i].value == 1) {
-					this->cards[i].value = 11;
+		int total = this->total();
+		size_t handSize = this->cards.size(); //size_t to avoid "possible loss of data"
+		int aces = this->aceCount;
+
+		if (total < 21) {
+			while (aces-- && (total + 10 <= 21)) {
+				for (int i = 0; i < handSize; i++) {
+					if (this->cards[i].name.find("Ace") != std::string::npos) {
+						this->cards[i].value = 11;
+					}
 				}
 			}
 		}
+		else if (total > 21) {
+			while (aces-- && (total > 21)) {
+				for (int i = 0; i < handSize; i++) {
+					if (this->cards[i].name.find("Ace") != std::string::npos) {
+						this->cards[i].value = 1;
+					}
+				}
+			}
+		}
+		else if (total == 21) { return; }
 	}
 
 	//constructors
@@ -193,14 +206,15 @@ void generateDeck(std::vector<Card>& deck) {
 }
 
 void showHands(Hand& visibleHand, Hand& hiddenHand) {
-	std::cout << "============================================================================\n";
+	std::cout << std::endl;
 	visibleHand.show();
 	hiddenHand.show(0);
-	std::cout << "\n============================================================================\n";
+	std::cout << std::endl;
 }
 
 void askInsurance(Player& player) {
-	std::cout << "Dealer's has an ace. Enter insurance bet (or 0 for none): ";
+	std::cout << "Dealer's has an ace. Enter insurance bet ";
+	std::cout << "(max " << player.bet/2 << " or 0 for none) : ";
 	std::cin >> player.insuranceBet;
 	while (player.insuranceBet > player.bet / 2) {
 		std::cout << "Insurance bet must be no higher than half the original bet (" << player.bet << ")!\n";
@@ -208,6 +222,7 @@ void askInsurance(Player& player) {
 	}
 	if (player.insuranceBet != 0) {
 		player.insurance = 1;
+		std::cout << "Made insurance bet of " << player.insuranceBet << "!\n";
 	}
 }
 
@@ -215,25 +230,33 @@ void insuranceCheck(bool& insurance, int& insuranceBet, Player& player) {
 	if (insurance == 1) {
 		int insuranceWin = insuranceBet * 2;
 		player.balance += insuranceWin;
-		std::cout << "Won " << insuranceWin << "$ from insurance side bet!";
+		std::cout << "Won " << insuranceWin << "$ from insurance side bet!\n";
 	}
 }
 
 bool checkBlackjack(Player& player, Player& dealer, bool& insurance, int& insuranceBet) {
 	bool blackjack = 1;
 	if (player.hand.total() == 21 && dealer.hand.total() == 21) {
-		std::cout << "PUSH - PLAYER AND DEALER BLACKJACK\n";
-		std::cout << "Bet of " << player.bet << " returned.\n";
+		std::cout << "DRAW (Double blackjack) " << "Bet of " << player.bet << " returned.\n";;
 		player.balance += player.bet;
 		insuranceCheck(insurance, insuranceBet, player);
+		std::cout << std::endl;
+		dealer.hand.show();
+		std::cout << std::endl;
 	}
 	else if (player.hand.total() == 21) {
 		std::cout << "PLAYER BLACKJACK!! You won " << player.bet + (player.bet*3) / 2 << "$!\n";
 		player.balance += player.bet + (player.bet * 3) / 2;
+		std::cout << std::endl;
+		dealer.hand.show();
+		std::cout << std::endl;
 	}
 	else if (dealer.hand.total() == 21) {
-		std::cout << "Dealer blackjack. ";
+		std::cout << "DEALER BLACKJACK! Lost "<< player.bet << "$ main bet.\n";
 		insuranceCheck(insurance, insuranceBet, player);
+		std::cout << std::endl;
+		dealer.hand.show();
+		std::cout << std::endl;
 	}
 	else {
 		blackjack = 0;
@@ -246,32 +269,45 @@ void roundOutcome(Player& player, Player& dealer, char choice) {
 		return;
 	}
 	if (choice == 'u') {
-		std::cout << "\nError: No choice\n";
+		std::cout << "\nError: No choice\n\n";
 		return;
 	}
 	int playerHand = player.hand.total();
 	int dealerHand = dealer.hand.total();
 
+	std::string compare = "HANDS - PLAYER: " + std::to_string(playerHand) + "  DEALER: " + std::to_string(dealerHand) + "\n";
+
 	if (dealerHand > 21) {
-		std::cout << "Dealer BUST, you win " << player.bet * 2 << "$!\n";
+		std::cout << "YOU WIN " << player.bet * 2 << "$! (dealer bust) " << compare << "\n\n";
 		player.balance += player.bet * 2;
 		return;
 	}
 	else if (playerHand > 21) {
-		std::cout << "Player BUST, bet lost.\n";
+		std::cout << "YOU LOSE " << player.bet << "(player bust) " << compare << "\n\n";
 		return;
 	}
 
 	if (playerHand == dealerHand) {
-		std::cout << "PUSH - Hands value is equal.\n";
+		std::cout << "DRAW (equal hands) " << "Bet of " << player.bet << " returned." << compare << "\n\n";
 		player.balance += player.bet;
 	}
 	else if (playerHand > dealerHand) {
-		std::cout << "PLAYER WINS! You won " << player.bet * 2 << "$!\n";
+		std::cout << "YOU WIN " << player.bet * 2 << "$! (hand higher than dealer) " << compare << "\n\n";
 		player.balance += player.bet * 2;
 	}
 	else if (playerHand < dealerHand) {
-		std::cout << "Dealer wins.\n";
+		std::cout << "YOU LOSE " << player.bet << "(dealer hand is higher) " << compare << "\n\n";
+	}
+}
+
+void dealerAutoplay(Player& player, Player& dealer, std::vector<Card>& deck) {
+	if (player.hand.total() == 21) {
+		return;
+	}
+	while (dealer.hand.total() < 17) {
+		dealer.hand.draw(deck);
+		dealer.hand.show(0);
+		std::cout << std::endl;
 	}
 }
 
@@ -279,7 +315,7 @@ int main()
 {
 	int minimumBet = 10;
 	std::vector<Card> deck;
-	Player player = { "Player" }, dealer = { "Dealer" };
+	Player player = { "PLAYER" }, dealer = { "DEALER" };
 	player.balance = 1000;
 
 	//game loop
@@ -299,7 +335,7 @@ int main()
 		std::cout << std::endl;
 
 		//if dealer has ace, ask if player wants insurance
-		if (dealer.hand.cards[0].value == 1) {
+		if (dealer.hand.cards[0].name.find("Ace") != std::string::npos) {
 			askInsurance(player);
 		}
 
@@ -313,8 +349,10 @@ int main()
 		char choice = 'u';
 		while (player.hand.total() <= 21 && dealer.hand.total() <= 21) {
 			turn++;
-			std::cout << ((turn == 1) ? "Options: Hit - h, Stand - s, Double-down - d, Surrender - x\n" : "Options: Hit - h, Stand - s\n");
-			std::cout << "Enter choice: ", std::cin >> choice; //s (or any other input) = do nothing
+			
+			std::string options = ((turn == 1) ? ("Hit - h, Stand - s, Double-down - d, Surrender - x") : "Hit - h, Stand - s");
+			std::cout << "[Bet $" << player.bet << "] ";
+			std::cout << "Select choice (" << options << "): ", std::cin >> choice;
 			
 			if (choice == 'x') {
 				player.balance += player.bet / 2;
@@ -328,13 +366,8 @@ int main()
 				player.bet += player.bet;
 				player.hand.draw(deck);
 				std::cout << "Bet increased to: " << player.bet << ".\n";
-				if (player.hand.total() == 21) {
-					break;
-				}
-				while (dealer.hand.total() < 17) {
-					dealer.hand.draw(deck);
-					dealer.hand.show(0);
-				}
+				player.hand.show();
+				dealerAutoplay(player, dealer, deck);
 				break; //ends the round loop after dealer finishes drawing
 			}
 			else if (turn != 1) {
@@ -345,12 +378,17 @@ int main()
 				}
 			} //end of double down
 
+			if (choice == 's') {
+				dealerAutoplay(player, dealer, deck);
+				break;
+			}
+
 			if (choice == 'h') {
 				player.hand.draw(deck);
 			}
 
 			int dealerHand = dealer.hand.total();
-			if (dealerHand >= 17 && choice != 'h') {
+			if (dealerHand >= 17 && choice == 's') {
 				break;
 			}
 			if (dealerHand < 17) {
@@ -361,7 +399,9 @@ int main()
 
 		} //end of turn loop
 
+		std::cout << std::endl;
 		dealer.hand.show();
+		std::cout << std::endl;
 
 		roundOutcome(player, dealer, choice);
 
